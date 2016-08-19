8affc01e931df134f7b15d7fe371acaea823a8ad
Armon Shariati <armon.shariati@gmail.com>
2016-08-19 15:55:52 -0400

    We need to think about the smartest way about doing this. I'm thinking to publish a blinker_tracker_info like message that maintains key-value pairs which associates an ekfs position in the particle array with a unique id

--- src/blinker_decoder.cpp
+++ src/blinker_decoder.cpp
@@ -1,10 +1,20 @@
 #include <ros/ros.h>
 
+#include <blinker_tracking/BlinkersWithImage.h>
+
+void callback(const blinker_tracking::BlinkersWithImage::ConstPtr &msg)
+{
+}
 
 int main(int argc, char* argv[])
 {
     ros::init(argc, argv, "blinker_decoder");
     ros::NodeHandle nh("~");
 
+    ros::Subscriber sub;
+    sub = nh.subscribe("blinkers", 10, &callback);
+
+    ros::spin();
+    return 0;
 
 }
