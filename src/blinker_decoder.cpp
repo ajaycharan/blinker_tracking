c13348fb9454252761e79d02549ee81dc0601a32
Armon Shariati <armon.shariati@gmail.com>
2016-08-22 17:18:21 -0400

    Added yaml support for the blinker database

--- src/blinker_decoder.cpp
+++ src/blinker_decoder.cpp
@@ -5,10 +5,12 @@
 
 #include <image_transport/image_transport.h>
 
+#include <fstream>
 #include <vector>
 #include <deque>
 #include <unordered_map>
 #include <algorithm>
+#include <yaml-cpp/yaml.h>
 
 #include <blinker_tracking/BlinkersWithImage.h>
 
@@ -17,6 +19,11 @@
  * matrices
  */
 
+struct BlinkerID
+{
+    unsigned int id;
+    unsigned int blink_code;
+};
 
 image_transport::Publisher debug_pub;
 
@@ -135,16 +142,52 @@ void blinker_callback(const blinker_tracking::BlinkersWithImage::ConstPtr &msg)
 
 }
 
+void load_blinker_database(std::string f, std::vector< BlinkerID >& blinker_ids)
+{
+    // open file
+    std::fstream fs;
+    fs.open(f.c_str());
+    if (!fs.is_open())
+    {
+        ROS_ERROR("Blinker database not found");
+    }
+    YAML::Node map = YAML::LoadFile(f.c_str());
+    assert(map.IsSequence());
+
+    for (int i = 0; i < map.size(); i++)
+    {
+        BlinkerID bid;
+        bid.id = (unsigned int) map[i]["id"].as<int>();
+        bid.blink_code = (unsigned int) map[i]["blink_code"].as<int>();
+        blinker_ids.push_back(bid);
+    }
+    
+}
+
 int main(int argc, char* argv[])
 {
     ros::init(argc, argv, "blinker_decoder");
     ros::NodeHandle nh("~");
     image_transport::ImageTransport it(nh);
 
+    // load parameters
+    std::string blinker_database;
+    nh.param(std::string("blinker_database"), blinker_database, std::string(""));
     nh.param(std::string("vote_thresh"), vote_thresh, 30);
     nh.param(std::string("intensity_change_thresh"), intensity_change_thresh, 200);
     nh.param(std::string("width"), width, 80.0);
     nh.param(std::string("height"), height, 80.0);
+
+    // load blinker database
+    std::vector< BlinkerID > blinker_ids;
+    load_blinker_database(blinker_database, blinker_ids);
+    
+    for(int i = 0; i < blinker_ids.size(); i++)
+    {
+        std::cout << blinker_ids[i].id << std::endl;
+        std::cout << blinker_ids[i].blink_code << std::endl;
+    }
+    std::cout << std::endl;
 
     ros::Subscriber blinker_sub;
     blinker_sub = nh.subscribe("blinkers", 10, &blinker_callback);
