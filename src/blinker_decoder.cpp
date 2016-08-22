6c2c71c80f43665bf4466429a2678e91cd5b438b
Armon Shariati <armon.shariati@gmail.com>
2016-08-22 15:14:37 -0400

    Almost there with the temporal decoding

--- src/blinker_decoder.cpp
+++ src/blinker_decoder.cpp
@@ -15,6 +15,8 @@
 image_transport::Publisher debug_pub;
 
 // parameter
+int intensity_change_thresh;
+int vote_thresh;
 double width;
 double height;
 
@@ -46,7 +48,7 @@ void blinker_callback(const blinker_tracking::BlinkersWithImage::ConstPtr &msg)
         subframes[id].push_front(patch);
 
         // maintain size
-        if (subframes[id].size() < 16)
+        if (subframes[id].size() < 18)
         {
             continue;
         }
@@ -58,31 +60,47 @@ void blinker_callback(const blinker_tracking::BlinkersWithImage::ConstPtr &msg)
         
         unsigned int even = 0;
         unsigned int odd = 0;
-        for (int j = 0; j < 6; j++)
+        for (int j = 0; j < 7; j++)
         {
             // compute intensity change
-            cv::Mat D_even;
-            cv::Mat D_odd;
-            cv::subtract(subframes[id][2*j  ], subframes[id][2*j+2], D_even);
-            cv::subtract(subframes[id][2*j+1], subframes[id][2*j+3], D_odd);
+            cv::Mat m_even;
+            cv::Mat m_odd;
+            cv::subtract(subframes[id][2*j  ], subframes[id][2*j+2], m_even);
+            cv::subtract(subframes[id][2*j+1], subframes[id][2*j+3], m_odd);
+
+            cv::Mat bm_even;
+            cv::Mat bm_odd;
+            cv::threshold(m_even, bm_even, intensity_change_thresh, 255, cv::THRESH_BINARY);
+            cv::threshold(m_odd, bm_odd, intensity_change_thresh, 255, cv::THRESH_BINARY);
+
+            // ratio of high to low pixels
+            int p_even = cv::countNonZero(bm_even);
+            int p_odd = cv::countNonZero(bm_odd);
+
+            // signal
+            unsigned int s_even = (p_even > vote_thresh) ? 1 : 0;
+            unsigned int s_odd = (p_odd > vote_thresh) ? 1 : 0;
+
+            // append to patterns list
+            even = even + (s_even << j);
+            odd = odd + (s_odd << j);
 
             // publish image to test 
             if (j == 0)
             {
                 cv_bridge::CvImage out;
                 out.encoding = std::string("mono8");
-                out.image = D_even;
+                out.image = bm_even;
 
                 sensor_msgs::ImagePtr image_ros = out.toImageMsg();
                 image_ros->header.stamp = ros::Time::now();
                 debug_pub.publish(image_ros);
             }
 
-            // blob detector
-
-            // append to patterns list
-
         }
+
+        std::cout << "Even Pattern : " << even << std::endl;
+        std::cout << "Odd Pattern : " << odd << std::endl;
 
         // pop oldest to maintain size
         subframes[id].pop_back();
@@ -122,6 +140,8 @@ int main(int argc, char* argv[])
     ros::NodeHandle nh("~");
     image_transport::ImageTransport it(nh);
 
+    nh.param(std::string("vote_thresh"), vote_thresh, 20);
+    nh.param(std::string("intensity_change_thresh"), intensity_change_thresh, 200);
     nh.param(std::string("width"), width, 50.0);
     nh.param(std::string("height"), height, 50.0);
 
